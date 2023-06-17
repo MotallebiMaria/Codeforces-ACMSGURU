354e0d28a8f9d99bb185bf092fe6bf4b4ecac60d
Maria Motallebi <motallebimaria@gmail.com>
2023-06-17 13:58:44 -0400

    WA on test 2

---
+++ 186/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MAXN = 1e2 + 10;
+int n, l[MAXN];
+ 
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> l[i];
+    cout << "1\n";
+    return 0;
+}
